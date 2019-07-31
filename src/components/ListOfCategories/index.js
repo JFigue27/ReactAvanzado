1e437d6a450ba421b399bd87259777892f1bef46
JoseJaime Figueroa <jaimefigueroa84@gmail.com>
2019-07-31 14:56:19 -0600

    First hook with categories.

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Category } from '../Category';
 import { List, Item } from './styles';
-import { categories } from '../../../api/db.json';
+// import { categories as mockCategories } from '../../../api/db.json';
 
 export const ListOfCategories = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(function name(params) {
+    window
+      .fetch('https://petgram-server-jjfigueroa.jjfigueroa.now.sh/categories')
+      .then(resp => resp.json())
+      .then(respons => {
+        setCategories(respons);
+      });
+  }, []);
+
   return (
     <List>
       {categories.map(category => (
